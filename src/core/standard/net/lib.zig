767ef6f581806863edc9cccf45a8df99617c4ccc
Sythivo <69376875+Sythivo@users.noreply.github.com>
2024-11-21 23:57:59 -0500

    Add TCP Socket support & tests
    - Added TCP Socket support, client and host.
    - Added tests for TCP Socket.
    - Added one more test for UDP Socket.

--- src/core/standard/net/lib.zig
+++ src/core/standard/net/lib.zig
@@ -7,7 +7,8 @@ const Scheduler = @import("../../runtime/scheduler.zig");
 
 const luaHelper = @import("../../utils/luahelper.zig");
 
-const Udp = @import("udp.zig");
+const UDP = @import("udp.zig");
+const TCP = @import("tcp.zig");
 const HttpServer = @import("httpserver.zig");
 const HttpClient = @import("httpclient.zig");
 const WebSocketClient = @import("websocket.zig");
@@ -17,11 +18,14 @@ pub const LIB_NAME = "net";
 pub fn loadLib(L: *Luau) void {
     HttpServer.lua_load(L);
     WebSocketClient.lua_load(L);
-    Udp.lua_load(L);
+    UDP.lua_load(L);
+    TCP.lua_load(L);
 
     L.newTable();
 
-    L.setFieldFn(-1, "udpsocket", Scheduler.toSchedulerEFn(Udp.lua_udpsocket));
+    L.setFieldFn(-1, "udpsocket", Scheduler.toSchedulerEFn(UDP.lua_udpsocket));
+    L.setFieldFn(-1, "tcpconnect", Scheduler.toSchedulerEFn(TCP.lua_tcp_client));
+    L.setFieldFn(-1, "tcphost", Scheduler.toSchedulerEFn(TCP.lua_tcp_server));
 
     L.setFieldFn(-1, "serve", Scheduler.toSchedulerEFn(HttpServer.lua_serve));
     L.setFieldFn(-1, "request", Scheduler.toSchedulerEFn(HttpClient.lua_request));
