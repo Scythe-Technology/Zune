5109227fe76051753327e3da2fbe3ac89518f6a1
Sythivo <69376875+Sythivo@users.noreply.github.com>
2024-11-25 13:28:56 -0500

    Improved init command with template
    - Added main and module template files.
    - Updated toml configuration file template.

--- src/commands/init.zig
+++ src/commands/init.zig
@@ -8,42 +8,102 @@ const INIT_CONFIG_FILE =
     \\# Debug settings
     \\#  This affects the way Zune handles errors and debug information.
     \\[runtime.debug]
-    \\detailedError=false
-    \\
-    \\# Require settings
-    \\#  This affects the way Zune loads files.
-    \\[resolvers.require]
-    \\mode="RelativeToFile"
+    \\detailedError = true
     \\
     \\# Formatter settings
     \\#  This affects the output of the formatter while printing.
     \\[resolvers.formatter]
-    \\maxDepth=4
-    \\useColor=true
-    \\showTableAddress=true
-    \\showRecursiveTable=false
-    \\displayBufferContentsMax=48
+    \\maxDepth = 4
+    \\useColor = true
+    \\showTableAddress = true
+    \\showRecursiveTable = false
+    \\displayBufferContentsMax = 48
     \\
     \\# Compiling settings
     \\#  This affects all required files and the main file.
     \\[compiling]
-    \\debugLevel=2
-    \\optimizationLevel=1
-    \\nativeCodeGen=true
+    \\debugLevel = 2
+    \\optimizationLevel = 1
+    \\nativeCodeGen = true
+    \\
+    \\# Experimental settings
+    \\#  This affects the experimental features of Zune.
+    \\[experimental]
+    \\ffi = false
+    \\sqlite = false
     \\
     \\# FFlag settings for Luau
     \\#  You can use `zune luau list-fflags` to list all available FFlags.
-    \\# [luau.fflags]
-    \\# DebugCodegenOptSize=false
+    \\#[luau.fflags]
+    \\#DebugCodegenOptSize = false
 ;
 
-fn Execute(_: std.mem.Allocator, _: []const []const u8) !void {
-    const config_file = try std.fs.cwd().createFile("zune.toml", .{
+fn Execute(_: std.mem.Allocator, args: []const []const u8) !void {
+    const config_file = std.fs.cwd().createFile("zune.toml", .{
         .exclusive = true,
-    });
+    }) catch |err| switch (err) {
+        error.PathAlreadyExists => {
+            std.debug.print("Zune configuration file, 'zune.toml' already exists.\n", .{});
+            return;
+        },
+        else => return err,
+    };
     defer config_file.close();
 
     try config_file.writeAll(INIT_CONFIG_FILE);
+
+    try std.fs.cwd().makePath("src");
+
+    if (args.len > 0 and std.mem.eql(u8, args[0], "module")) {
+        const file = std.fs.cwd().createFile("src/init.luau", .{
+            .exclusive = true,
+        }) catch |err| switch (err) {
+            error.PathAlreadyExists => return,
+            else => return err,
+        };
+        defer file.close();
+
+        try file.writeAll(
+            \\local stdio = zune.stdio
+            \\
+            \\local module = {}
+            \\
+            \\function module.hello()
+            \\    stdio.stdout:write("Hello, World!")
+            \\end
+            \\
+            \\if zune.testing.running then
+            \\    local testing = zune.testing
+            \\
+            \\    local test = testing.test
+            \\
+            \\    test("module.hello", function()
+            \\        module.hello()
+            \\    end)
+            \\end
+            \\
+            \\return module
+        );
+    } else {
+        const file = std.fs.cwd().createFile("src/main.luau", .{
+            .exclusive = true,
+        }) catch |err| switch (err) {
+            error.PathAlreadyExists => return,
+            else => return err,
+        };
+        defer file.close();
+
+        try file.writeAll(
+            \\local stdio = zune.stdio
+            \\
+            \\local function main()
+            \\    stdio.stdout:write("Hello, World!")
+            \\end
+            \\
+            \\main()
+            \\
+        );
+    }
 }
 
 pub const Command = command.Command{
