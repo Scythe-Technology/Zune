672c79cafc489614234c406d00c68d6f2cf8396b
Sythivo <69376875+Sythivo@users.noreply.github.com>
2025-06-21 21:06:14 -0400

    Add glob api and re-enable x86_64 macOS watch test

--- src/core/standard/fs/lib.zig
+++ src/core/standard/fs/lib.zig
@@ -664,6 +664,15 @@ const Path = struct {
         L.pushboolean(fs.path.isAbsolute(path));
         return 1;
     }
+
+    pub fn lua_globMatch(L: *VM.lua.State) !i32 {
+        const path = try L.Zcheckvalue([:0]const u8, 1, null);
+        const pattern = try L.Zcheckvalue([:0]const u8, 2, null);
+
+        L.pushboolean(Zune.glob.match(pattern, path).matches());
+
+        return 1;
+    }
 };
 
 pub fn loadLib(L: *VM.lua.State) void {
@@ -707,6 +716,7 @@ pub fn loadLib(L: *VM.lua.State) void {
         .stem = Path.lua_stem,
         .extension = Path.lua_extension,
         .isAbsolute = Path.lua_isAbsolute,
+        .globMatch = Path.lua_globMatch,
     });
     L.setreadonly(-1, true);
     L.setfield(-2, "path");
